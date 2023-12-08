b5a76509c173ef2b2b7c01f4416dc152960cf7f3
Austin Vichich <avichich4@gmail.com>
2023-12-08 10:26:51 -0700

    displaying resort names on page

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 
 
 //API fetch call
+function fetchAllData(){
 fetch("http://localhost:3000/resortData")
-.then((response) => response.json())
-.then((weatherData) => {
-    console.log(weatherData)
+.then(response => response.json())
+.then(resortData => {
+    const resortDataDiv = document.getElementById('weather-summary')
+
+    resortData.map(resort => {
+    const cardDiv = document.createElement('div')
+    cardDiv.className = 'resort-card'
+
+    const resortName = document.createElement('h2')
+    resortName.textContent = resort.name
+    cardDiv.appendChild(resortName)
+
+    resortDataDiv.appendChild(cardDiv)
+   })
 });
+}
+fetchAllData()
